9f1f0822b7ff026e4b611871f8915f5657a9d6d5
mohamadhashish <mohmad.nzhat.h@gmail.com>
2025-09-23 15:39:10 +0200

    g

---
+++ Uebungen/130_Merkbares_Zufallspasswort.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Merkbares Zufallspasswort
+ *
+ * Für eine Webseite muss ein Passwort erzeugt werden.
+ * Du hast gemerkt, dass eine zufällige Folge von Buchstaben,
+ * Ziffern und Sonderzeichen leicht zu progarmmieren,
+ * jedoch schlecht zu merken ist.
+ *
+ * Nun mache folgende Vorgaben an ein automatisch generientes Passwort.
+ * Das Passwort muss insgesamt mindestens sechs, maximal zehn Zeichen enthalten.
+ * Das Passwort besteht aus einem "Wort",
+ * gefolgt von genau einem Sonderzeichen und danach aus einer Zahl.
+ * Das "Wort" hat vier, fünf oder sechs Buchstaben,
+ * wobei nur der erste ein Großbuchstabe sein darf.
+ * Die Zahl hat eine, zwei oder drei Stellen.
+ * Im "Wort" wechseln Konsonanten mit Vokalen immer ab.
+ * Verwechselbare Zeichen sollen nicht vorkommen.
+ *
+ * Schreibe ein Programm, das ein "merkbares" Zufallspasswort ausgibt.
+ */
---
+++ Uebungen/140_Teilersumme.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Teilersumme
+ *
+ * Schreibe eine Funktion, die überprüft, ob bei einer Zahl
+ * die Summe aller Teiler kleiner als die Zahl ist.
+ * Die Zahl selber soll hierbei nicht zu den Teilern zählen.
+ *
+ * Bei 81 würde TRUE zurück gegeben werden, da
+ * 1 + 3 + 9 + 27 = 40
+ * und 40 < 81
+ *
+ * Bei 80 würde FALSE zurück gegeben werden, da
+ * 1 + 2 + 4 + 5 + 8 + 10 + 16 + 20 + 40 = 106
+ * und 106 > 80
+ */
+
